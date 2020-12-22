102b285e93513375fff7af3be513818a3f61f52c
Hiro Ayettey <hiro.ayettey@gmail.com>
2020-12-22 16:54:17 -0500

    Init with gatsby

---
+++ src/Descriptions/IconDescription.js
@@ -0,0 +1,30 @@
+import resumeIllustration from "../images/resume.svg"
+import workExperienceIllustration from "../images/workExperience.svg"
+import projectsIllustration from "../images/projects.svg"
+import resume from "../images/resume.pdf"
+
+function IconDescription(text, image, link) {
+    this.image = image;
+    this.text = text;
+    this.link = link
+}
+
+var aboutDescriptions = [
+    new IconDescription(
+        "Resume",
+        resumeIllustration,
+        resume
+    ),
+    new IconDescription(
+        "Work Experience",
+        workExperienceIllustration,
+        "/workExperience"
+    ),
+    new IconDescription(
+        "Projects",
+        projectsIllustration,
+        "/projects"
+    )
+]
+
+export default aboutDescriptions;
