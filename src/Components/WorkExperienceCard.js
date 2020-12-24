8c40255a8589649938cbb9f2fe2f18a299e962d7
Hiro Ayettey <hiro.ayettey@gmail.com>
2020-12-23 20:23:37 -0500

    Reverse work experience and add some animation

--- src/Components/WorkExperienceCard.js
+++ src/Components/WorkExperienceCard.js
@@ -5,7 +5,7 @@ import '../CSS/App.css'
 
 function WorkExperienceCard(jobDescription, key) {
     return (
-        <GridItem rowSpan={{ base: 2, sm: 4, md: 2, lg: 2, xl: 2 }} colSpan={{ base: 18, sm: 14, md: 19, lg: 19, xl: 19 }} mb={{ base: "3vh" }} key={key}>
+        <GridItem rowSpan={{ base: 2, sm: 2 }} colSpan={{ base: 18, sm: 14, md: 19, lg: 19, xl: 19 }} mb={{ base: "3vh" }} key={key}>
             <div className="WorkExperienceCardContainer">
                 <div className="WorkExperienceTextContainer">
                     <HStack className="WorkExperienceCard" spacing="2vw">
@@ -15,7 +15,7 @@ function WorkExperienceCard(jobDescription, key) {
                                 <Text className="WorkExperienceTextTitle" fontSize={{ base: "30px", md: "40px", lg: "40px" }} >{jobDescription.title}</Text>
                             </a>
                             <Text className="WorkExperienceTextPosition" fontSize={{ base: "20px", md: "20px", lg: "20px" }}>{jobDescription.jobPosition} - {jobDescription.location}</Text>
-                            <Text>{jobDescription.text}</Text>
+                            <Text fontSize={{ base: "17px", md: "20px", lg: "17px" }}>{jobDescription.text}</Text>
                         </Stack>
                     </HStack>
                 </div>
--- src/pages/work-experience.js
+++ src/pages/work-experience.js
@@ -25,25 +25,24 @@ function WorkExperience() {
     for (var i = 0; i < jobDescriptions.length; i++) {
         jobCards.push(WorkExperienceCard(jobDescriptions[i], i))
     }
-
     return (
         <ChakraProvider>
             <div className="App">
                 {MetaTags("Work Experience")}
-                <Grid className="WorkExperience" templateRows="repeat(15, 1fr)" templateColumns="repeat(20, 1fr)" flexWrap="wrap">
+                <Grid className="WorkExperience" templateRows="repeat(15, 1fr)" templateColumns="repeat(20, 1fr)" >
                     <GridItem colStart={2} rowStart={1} colSpan={{ base: 0, md: 12 }}>
                         <Text fontSize={{ base: "50px", md: "70px", lg: "73px" }} className="WorkExperienceTitle">
                             Work Experience
                         </Text>
                     </GridItem>
-                    <GridItem rowStart={2} colStart={1} colSpan={{ base: 19, md: 19 }}>
+                    <GridItem colSpan={{ base: 19, md: 19 }}>
                         <Steps progressDot direction="horizontal" className="WorkExperienceTimeline" size="default">
                             <Step title="May-Aug 2019" description="Threads" status="finish" className="WorkExperienceTimelineItem" />
                             <Step title="Jan-Apr 2020" description="TextNow" status="finish" className="WorkExperienceTimelineItem" />
                             <Step title="Sept-Dec 2020" description="TD Labs" status="finish" className="WorkExperienceTimelineItem" />
                         </Steps>
                     </GridItem>
-                    {jobCards}
+                    {jobCards.reverse()}
                 </Grid>
                 {ExternalSiteCard(linkedInDescription)}
                 {HomeCard()}
