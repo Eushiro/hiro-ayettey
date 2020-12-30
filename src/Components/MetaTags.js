d9de0df19d1d7364dbe625e10012ba1d346d7e15
Hiro Ayettey <hiro.ayettey@gmail.com>
2020-12-29 19:37:37 -0500

    Update MetaTags.js

--- src/Components/MetaTags.js
+++ src/Components/MetaTags.js
@@ -7,14 +7,27 @@ function MetaTags(pageTitle, url) {
   var description = "Hi I'm Hiro, a Software Engineering student at the University of Waterloo. This is my personal website describing some of my work experience and projects."
   return (
     <Helmet>
+      {/* HTML Meta Tags */}
       <title>{title}</title>
-      <meta charSet="utf-8" name="description" content={description}
-      />
-      <meta property="og:title" content={title} />
-      <meta property="og:type" content="website" />
+      <meta name="description" content={description} />
+
+      {/* Google / Search Engine Tags */}
+      <meta itemprop="name" content={title} />
+      <meta itemprop="description" content={description} />
+      <meta itemprop="image" content={profile} />
+
+      {/* Facebook Meta Tags */}
       <meta property="og:url" content={url} />
-      <meta property="og:image" content={profile} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <meta property="og:image" content={profile} />
+
+      {/* Twitter Meta Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={profile} />
     </Helmet>
   );
 }
